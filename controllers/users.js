22b4f43e97a1f69cb5a0727c74044b3f917240fc
emelipasini <emelipasini@outlook.com>
2020-05-11 16:53:16 -0300

    Agrego registro y login con rutas y controladores de usuarios

--- controllers/users.js
+++ controllers/users.js
@@ -1,50 +1,65 @@
 let users = require("../data/users.json");
 const fs = require("fs");
 const bcrypt = require("bcrypt");
+const { check, validationResult, body } = require("express-validator");
 
 const userController = {
     create: function (req, res) {
         res.render("register");
     },
     save: function (req, res) {
-        users.forEach(user => {
-            if(user.email == req.body.email) {
-                res.send("El usuario ya existe.");
-            }
-        });
-        let lastId = users[users.length - 1].id;
-        let password = bcrypt.hashSync(req.body.password, 10);
 
-        users.push(
-            {
-                "id": lastId + 1,
-                "name": req.body.name,
-                "surname": req.body.surname,
-                "email": req.body.email,
-                "password": password
-                // "avatar": req.files[0].filename
-            }
-        );
-        fs.writeFileSync("./data/users.json", JSON.stringify(users));
-        res.redirect("/usuario/perfil");
+        let errors = validationResult(req);
+        if(errors.isEmpty()) {
+            users.forEach(user => {
+                if(user.email == req.body.email) {
+                    errors.errors.push({ msg: "El usuario ya existe" })
+                    res.render("register", { errors: errors.errors });
+                }
+            });
+            let lastId = users[users.length - 1].id;
+            let password = bcrypt.hashSync(req.body.password, 10);
+    
+            users.push(
+                {
+                    "id": lastId + 1,
+                    "name": req.body.name,
+                    "surname": req.body.surname,
+                    "email": req.body.email,
+                    "password": password
+                    // "avatar": req.files[0].filename
+                }
+            );
+            fs.writeFileSync("./data/users.json", JSON.stringify(users));
+            res.redirect("/usuario/perfil");
+        }
+        else {
+            return res.render("register", { errors: errors.errors, data: req.body });
+        }
+
     },
     logForm: function (req, res) {
         res.render("login");
     },
     login: function (req, res) {
-        users.forEach(user => {
-            if(user.email == req.body.email) {
-                let validate = bcrypt.compareSync(req.body.password, user.password);
-                console.log(validate);
-                if(validate) {
-                    res.redirect("/usuario/perfil");
+
+        let errors = validationResult(req);
+        if(errors.isEmpty()) {
+            users.forEach(user => {
+                if(user.email == req.body.email) {
+                    let validate = bcrypt.compareSync(req.body.password, user.password);
+                    console.log(validate);
+                    if(validate) {
+                        res.redirect("/usuario/perfil");
+                    }
                 }
-                else {
-                    res.send("Los datos no son correctos.");
-                }
-            }
-        });
-        res.send("Los datos no son correctos,");
+            });
+            errors.errors.push({ msg : "Credenciales incorrectas" })
+            res.render("login", { errors: errors.errors, data: req.body });
+        }
+        else {
+            res.render("login", { errors: errors.errors, data: req.body });
+        }
     },
     profile: function (req, res) {
         res.render("profile");
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,39 @@
 var express = require('express');
 var router = express.Router();
 const usersController = require("../controllers/users");
+const { check, validationResult, body } = require("express-validator");
 
 // GET
 // registracion
-router.get("/crear", usersController.create);
+router.get("/registro", usersController.create);
 // renderiza el formulario de login
 router.get("/ingresar", usersController.logForm);
 // renderiza el perfil del usuario
 router.get("/perfil", usersController.profile);
 
 // POST
+
 // guarda el nuevo usuario
-router.post("/crear", usersController.save);
+router.post("/registro", [
+    check("name").trim().isLength({ min: 3 }).withMessage("El nombre es obligatorio")
+    .isAlpha().withMessage("El nombre no puede contener números"),
+
+    check("surname").trim().isLength({ min: 3 }).withMessage("El apellido es obligatorio")
+    .isAlpha().withMessage("El apellido no puede contener números"),
+
+    check("email").trim().isEmail().withMessage("El correo no es válido"),
+
+    check("password").trim().isLength({ min: 8 }).withMessage("La contraseña debe contener al menos 8 caracteres")
+    .isAlphanumeric().withMessage("La contraseña debe contener letras y números"),
+    
+    check("r-pass", "Las contraseñas no coinciden").trim().custom((value, { req }) => (value === req.body.password))
+
+], usersController.save);
+
 // Ingresa al perfil
-router.post("/ingresar", usersController.login);
+router.post("/ingresar", [
+    check("email").trim().isEmail().withMessage("El correo no es válido"),
+    check("password").trim().isLength().withMessage("La contraseña es obligatoria")
+], usersController.login);
 
 module.exports = router;
